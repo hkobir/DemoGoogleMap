b6e14845bb1d2ef9cb39c195a101853ada66f6f2
hkobir <hkobir10@gmail.com>
2019-10-28 15:36:33 +0600

    add a marker with demo latitude and longitude

--- app/src/main/java/com/hk/demogooglemap/MainActivity.java
+++ app/src/main/java/com/hk/demogooglemap/MainActivity.java
@@ -3,9 +3,13 @@ package com.hk.demogooglemap;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -19,6 +23,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        
+        LatLng nangalkot = new LatLng(23.17, 91.20);
+        googleMap.addMarker(new MarkerOptions().position(nangalkot)).setTitle("Nangalkot Home Town");
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(nangalkot,16));
+
+
+
     }
 }
