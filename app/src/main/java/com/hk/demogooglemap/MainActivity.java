a42dc2059254ae72ec84f1204950fb2e7e6ebeab
hkobir <hkobir10@gmail.com>
2019-10-28 15:05:37 +0600

    add dependency of project and initialize fragment

--- app/src/main/java/com/hk/demogooglemap/MainActivity.java
+++ app/src/main/java/com/hk/demogooglemap/MainActivity.java
@@ -1,13 +1,24 @@
 package com.hk.demogooglemap;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        
     }
 }
